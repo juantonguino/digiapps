ebeb4efea22e7e6163ddab75539d52945bd3fb85
juantonguino <juan.tonguino@gmail.com>
2020-11-14 15:13:55 -0500

    Se modifica el archivo enviroments en producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,12 @@
 export const environment = {
   production: true,
-
+  firebaseConfig:{
+    apiKey: "AIzaSyBVX9_I8iCiez8AH_bfuw5t6rHezIzogkQ",
+    authDomain: "digiapps-51eb8.firebaseapp.com",
+    databaseURL: "https://digiapps-51eb8.firebaseio.com",
+    projectId: "digiapps-51eb8",
+    storageBucket: "digiapps-51eb8.appspot.com",
+    messagingSenderId: "sender-id",
+  },
   redirectSite: "http://ec2-3-85-9-193.compute-1.amazonaws.com/gracias-por-registrarte/"
 };
